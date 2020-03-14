a31ff2c5f9a47037f6c47848dac52c22880cf38b
depsterr <dragonvale09@gmail.com>
2020-03-14 16:37:34 +0100

    started work on responses

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,85 @@
 use std::net::{TcpListener, TcpStream};
-use std::sync::{Mutex, Arc};
 use std::io::prelude::*;
-use std::time::Duration;
-use std::thread;
-use std::char;
+use std::process::exit;
+use std::str;
+use std::fmt;
+use std::env;
+use std::fs;
 
-const MAX_CLIENTS:i32 = 50;
+const VERSION:&str = "0.1.0";
+const DIRECTORY:&str = "/var/www/html";
+const ALLOWSYM:bool = false;
 
+/// This is a function that takes a TcpStream, reads data
+/// from it and then sends a response.
 fn
-handle_client (mut stream: TcpStream, client_lock: Arc<Mutex<i32>>) {
-    let mut clients = client_lock.lock().unwrap();
-    let mut data = [0; 128];
+handle_client (mut stream: TcpStream) -> Result<(), ()> {
+    println!("======= New request =======\n");
+
+    let mut data = [0; 2048];
     stream.read(&mut data).unwrap();
-    println!("Request #{}:", *clients);
-    for x in data.iter() {
-        let c:char = char::from_u32(*x as u32).unwrap();
-        print!("{}", c);
+
+    let data_string = match str::from_utf8(&data) {
+        Ok(v) => v,
+        Err(_e) => return Err(())
+    };
+
+    print!("{}", data_string);
+
+    println!("\n======= End of request =======\n");
+
+    let data_splits: Vec<&str> = data_string.split_whitespace().collect();
+
+    if data_splits[0] != "GET" {
+        return Err(())
     }
-    *clients -= 1;
-    println!("exiting thread");
+
+    let mut filepath = String::from(DIRECTORY);
+    filepath.push_str(data_splits[1]);
+
+    /* TODO SANITIZE FILEPATH HERE */
+
+    let metadata = metadata(filepath)?;
+
+    if !metadata.is_ok()
+        err404();
+
+    if !(metadata.filetype().is_symlink() && ALLOWSYM)
+        err404();
+    
+    println!("Requesting file {}", filepath);
+
+    return Ok(())
 }
 
 fn
 main() {
-    // add some sort of arg handeling here
-    println!("Starting srws version 0.1.0");
+    let args: Vec<String> = env::args().collect();
 
-    let client_lock = Arc::new(Mutex::new(0));
-    let clients = Arc::clone(&client_lock);
-    
-    let listener = TcpListener::bind("localhost:80").unwrap();
+    let mut address = "127.0.0.1:80";
+
+    if args.len() > 1 {
+        address = &args[1];
+    }
+
+    println!("Starting srws version {}", VERSION);
+
+    let listener = match TcpListener::bind(address) {
+        Ok(v) => v,
+        Err(_e) => {
+            println!("Unable to start listening on '{}' did you run as root?", address);
+            exit(1);
+        }
+    };
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        stream.set_read_timeout(Some(Duration::from_secs(10)));
-
-        let clients = clients.lock().unwrap();
-
-        if *clients < MAX_CLIENTS {
-            thread::spawn( move || handle_client(stream, Arc::clone(&client_lock)));
-        } else {
-            while *clients == MAX_CLIENTS {
-                thread::sleep(Duration::from_secs(1));
-            }
-            thread::spawn( move || handle_client(stream, Arc::clone(&client_lock)));
+        let stream = match stream {
+            Ok(v) => v,
+            Err(_e) => continue,
+        };
+        match handle_client(stream) {
+            Ok(v) => v,
+            Err(_e) => continue,
         }
     }
 }
